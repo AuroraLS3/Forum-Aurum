545e1ab7e86e1beb6a1e7d20390b2cf94e6ae7d7
Rsl1122 <djrapitops@hotmail.com>
2018-03-20 16:50:09 +0200

    Moved app to package

--- forum/hello.py
+++ forum/hello.py
@@ -26,7 +26,3 @@ def hello():
 @app.route("/demo")
 def content():
     return render_template("demo.html", nimi=nimi, lista=lista, esineet=esineet)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
