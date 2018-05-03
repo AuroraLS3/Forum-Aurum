d8214087cc59eea562ae66f3bb09738a20196f1f
Rsl1122 <djrapitops@hotmail.com>
2018-05-03 12:41:04 +0300

    Improved markdown rendering

--- application/utils/CustomMisakaRenderer.py
+++ application/utils/CustomMisakaRenderer.py
@@ -4,6 +4,3 @@ from misaka import SaferHtmlRenderer
 class CustomMisakaRenderer(SaferHtmlRenderer):
     def blockquote(self, content):
         return '<blockquote class="blockquote">{}</blockquote>'.format(content)
-
-    def paragraph(self, content):
-        return content
